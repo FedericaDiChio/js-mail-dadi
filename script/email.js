1b94b2fca83c988a006e0a1ced9fe05fec7f716a
FedericaDiChio <federica.dichio7@gmail.com>
2021-07-11 13:11:51 +0200

    while

--- script/email.js
+++ script/email.js
@@ -9,27 +9,29 @@ stampa un messaggio appropriato sull’esito del controllo.
 var logIn = document.getElementById("log-in"); 
 
 // Creare lista di email che hanno un permesso
-var emailAllowed = ["marty55@gmail.com", "Emmett85@gmail.com", "Clara85@gmail.com"];
+var emailAllowed = ["marty55@gmail.com", "Emmett85@gmail.com", "Clara85@gmail.com", "Jenny80@gmail.com", "biff.T@gmail.com"];
 
 
 // Chiedere all'utente la sua mail
 var userEmail;
 
 do {
-    userEmail = prompt("Inserisci la tua e-mail");
+    userEmail = prompt("Inserisci la tua e-mail").trim();
 } while (!userEmail)
 
 // controllare se la mail dell'utente rientra tra le autorizzazioni 
 
 var isAllowed = false;
 
-for (i = 0; i < emailAllowed.length; i++) {
+i = 0;
+while (!isAllowed && i < emailAllowed.length) {
     if (userEmail === emailAllowed[i]) {
         isAllowed = true;
     }
     else {
         isAllowed;
     }
+    i++
 }
 
 // Stampiamo in pagina 
